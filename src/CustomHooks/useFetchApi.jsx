d952a1451e2e2d2d73ef1422dac973cb1da03478
Anubrata-01 <anubratachanda36@gmail.com>
2024-02-12 12:14:34 +0530

    make hooks for fetch api

---
+++ src/CustomHooks/useFetchApi.jsx
@@ -0,0 +1,19 @@
+import { useEffect,} from "react"
+import { useDispatch } from "react-redux"
+import { addMoodToday } from "../Redux Store/restaurantSlice";
+const useFetchApi=(foodApi)=>{
+    const dispatch=useDispatch();
+    useEffect(()=>{
+        const MoodData=async()=>{
+            const data=await fetch(foodApi);
+            const parseData=await data.json();
+            const filterData=
+              parseData?.data?.cards?.filter((item)=>item?.card?.card?.header?.title ==="What's on your mind?");
+         
+            dispatch(addMoodToday(filterData))
+            
+        }
+MoodData()
+    },[dispatch,foodApi])
+}
+export default useFetchApi;
