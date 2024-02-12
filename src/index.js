85e89bd2359cde9ed931b67229abc5675cd4262b
Anubrata-01 <anubratachanda36@gmail.com>
2024-02-12 12:15:29 +0530

    updated

--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import { Store } from './Redux Store/AppStore';
+import { Provider } from 'react-redux';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
+  <Provider store={Store}>
     <App />
-  </React.StrictMode>
+  </Provider>
 );
 
 
