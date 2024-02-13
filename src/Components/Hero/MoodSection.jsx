24a8a03fd856c7cf303d985245358c1416cb28d6
Anubrata-01 <anubratachanda36@gmail.com>
2024-02-13 19:20:58 +0530

    displayed items beautifully

--- src/Components/Hero/MoodSection.jsx
+++ src/Components/Hero/MoodSection.jsx
@@ -1,17 +1,15 @@
 import React from 'react';
 import useFetchApi from '../../CustomHooks/useFetchApi';
-import { CDN_url, foodApi } from '../../constant';
+import {  Swigy_url, foodApi } from '../../constant';
 import { useSelector } from 'react-redux';
+import styled from 'styled-components';
 
 const MoodSection = () => {
+  
   useFetchApi(foodApi);
-  const startTime = performance.now();
   const moodToday = useSelector(store => store?.restaurant?.moodToday);
   const headerTitle = React.useMemo(() => moodToday?.[0]?.card?.card?.header?.title || '', [moodToday]);
   const info= React.useMemo(() => moodToday?.[0]?.card?.card?.gridElements?.infoWithStyle?.info || '', [moodToday]);
-  const endTime = performance.now();
-  const executionTime = endTime - startTime;
-  console.log(`Execution time: ${executionTime} milliseconds`);
   console.log(headerTitle)
   if(!info) {
     return null
@@ -20,21 +18,59 @@ const MoodSection = () => {
 
 
   return (
-    <div>
-      <h1>{headerTitle}</h1>
-      <div>
+    <Container>
+      <Title>{headerTitle}</Title>
+      <ItemCon>
         {
-          info?.map((item)=>(
-            <div>
-          <img src={CDN_url+item?.imageId} alt="" />
-          <p>{item?.action?.text}</p>
-        </div>
+          info?.map((item,index)=>(
+            <Item key={index}>
+          <Img src={Swigy_url+item?.imageId} alt={item?.name} />
+        </Item>
           ))
         }
         
-      </div>
-    </div>
+      </ItemCon>
+    </Container>
   );
 }
+const Container=styled.div`
+width: 80%;
+margin-top:7%;
+margin-left:10%;
+box-sizing:border-box;
+`
+const  Title=styled.h1`
+font-family:Basis_Grotesque_Pro;
+font-size:22px;
+font-weight:bold;
+line-height: 28px;
+color:rgba(2, 6, 12, 0.92);
+`
+const ItemCon=styled.div`
+width:100%;
+overflow-x:scroll;
+scroll-behavior: smooth;
+display: flex;
+gap:18px;
+&::-webkit-scrollbar {
+    display: none;
+  }
+
+`
+
+const Img=styled.img`
+width:124px;
+height: 160px;
+
+`
+const Item=styled.div`
+display: flex;
+flex-direction: column;
+justify-content: center;
+align-items: center;
+cursor: pointer;
+
+`
+
 
 export default MoodSection;
