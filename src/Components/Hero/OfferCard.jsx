7df648e5f947f0cc1735ab30c47e97620e2cfa59
Anubrata-01 <anubratachanda36@gmail.com>
2024-02-29 12:51:26 +0530

    Created for show active offers

---
+++ src/Components/Hero/OfferCard.jsx
@@ -0,0 +1,55 @@
+import React from "react";
+import styled from "styled-components";
+
+const OfferCard = ({offerDetails}) => {
+    const{header,
+        couponCode,description
+
+        }=offerDetails?.info
+  return (
+    <Container>
+      <OfferCon>
+        <Offer>{header}</Offer>
+      </OfferCon>
+      <CouponCon>
+        <CuoponDesCription>{couponCode} |</CuoponDesCription>
+        <CuoponDesCription>{description}</CuoponDesCription>
+      </CouponCon>
+    </Container>
+  );
+};
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  border: 1px solid #e9e9eb;
+  box-sizing: border-box;
+  box-shadow: 0 1px 2px rgba(0, 0, 0, 0.04);
+  border-radius: 8px;
+  /* padding: 8px; */
+  /* height: 100%; */
+  min-width: 200px;
+  max-height: 60px;
+  align-items: center;
+`;
+const OfferCon = styled.div``;
+const Offer = styled.p`
+  font-size: 0.7rem;
+  font-weight: 600;
+  white-space: nowrap;
+  color: #686b78;
+  font-family: ProximaNovaCondensedBold, arial, Helvetica Neue, sans-serif;
+`;
+const CouponCon = styled.div`
+  margin-top: -10px;
+  display: flex;
+  align-items: center;
+  gap: 5px;
+`;
+const CuoponDesCription = styled.p`
+  font-size: 0.56rem;
+  font-weight: 700;
+  white-space: nowrap;
+  color: #686b78;
+  font-family: ProximaNovaCondensedBold, arial, Helvetica Neue, sans-serif;
+`;
+export default OfferCard;
---
+++ src/Components/Hero/OffersSection.jsx
@@ -0,0 +1,66 @@
+import React from 'react'
+import { useSelector } from 'react-redux';
+import styled from 'styled-components'
+import OfferCard from './OfferCard';
+import LineBar from '../../Utilities/LineBar';
+
+const OffersSection = ({cost,time}) => {
+  const cardDetails = useSelector((store) => store?.restaurant?.resCardDetails);
+  const Offers=React.useMemo(() =>
+  cardDetails?.data?.cards?.find(
+    (item) =>
+      item?.card?.card?.["@type"] ===
+       
+"type.googleapis.com/swiggy.gandalf.widgets.v2.GridWidget"
+
+  ),
+[cardDetails])
+Offers && console.log(Offers)
+  return (
+    <Container>
+        <div>
+            <TimeCon>
+            <Cost>{time?.slaString}</Cost>
+                <Cost>{cost}</Cost>
+                
+            </TimeCon>
+            <OfferCardContainer>{
+                Offers && Offers.card?.card?.gridElements?.infoWithStyle?.offers?.map((item)=>(
+                    <OfferCard offerDetails={item}/>
+                ))
+
+                
+                }
+                
+            </OfferCardContainer>
+        </div>
+       
+    </Container>
+  )
+}
+const Container=styled.div`
+    width:100%;
+    margin-left: 0%;
+    margin-top:1%;
+   
+`
+const TimeCon=styled.div`
+    margin-top:-8px;
+    display: flex;
+    gap:25px;
+`
+const Cost=styled.p`
+    font-size: 13px;
+    font-weight:900;
+
+`
+const OfferCardContainer=styled.div`
+width: 100%;
+overflow-x: scroll;
+display: flex;
+gap:5px;
+&::-webkit-scrollbar{
+    display: none;
+}
+`
+export default OffersSection
